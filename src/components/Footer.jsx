60a6e753a6d9ed2ba6298e4ce35f3463806decbf
ruddro420 <aliruddro@gmail.com>
2023-08-08 16:17:36 +0600

    formate ready

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer-container">
+        <div className="footer-content-container text-center text-white">
+          <h2>All Right Reserved By Tech I Trick @2023</h2>
+          <h4>Thank You !</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
